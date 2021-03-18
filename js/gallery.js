3b39a70d0841897651f362a79846b1f7bfbd7861
Вікторія <viktoriaa@MacBook-Air-Viktoria.local>
2021-03-18 15:10:55 +0200

    slider

--- js/gallery.js
+++ js/gallery.js
@@ -6,13 +6,59 @@ const modalWindowImage = document.querySelector('.lightbox__image');
 const closeButton = document.querySelector('[data-action="close-lightbox"]');
 const backdrop = document.querySelector('.lightbox__overlay');
 const galleryMarkup = createGalleryMarkup(galleryItems);
+let index; 
 
 galleryList.insertAdjacentHTML('beforeend', galleryMarkup);
 galleryList.addEventListener('click', onGalleryLinkClick);
-closeButton.addEventListener('click', onCloseGalleryButtonClick)
+closeButton.addEventListener('click', onCloseGalleryButtonClick);
 window.addEventListener('keydown', onEscapePress);
-backdrop.addEventListener('click', onBackdropCloseClick)
+backdrop.addEventListener('click', onBackdropCloseClick);
 
+function createGalleryMarkup(array) {
+    return array.map((image, index) => {
+        return `<li class="gallery__item">
+        <a
+          class="gallery__link"
+          href="${image.original}"
+        >
+          <img
+            class="gallery__image"
+            src="${image.preview}"
+            data-source="${image.original}"
+            alt="${image.description}"
+            data-index="${index}"
+          />
+        </a>
+      </li>`
+    }).join('')
+}
+
+
+function imagesSlider (images) {
+    window.addEventListener('keydown', function (event) {
+    // index = images.findIndex(item => item.original === modalWindowImage.src);
+    if (event.code === 'ArrowLeft') {
+        if (index === 0) {
+            index = images.length - 1;
+        } else {
+            index = index - 1;
+        }
+            modalWindowImage.src = galleryItems[index].original;
+       
+        }
+    
+     if (event.code === 'ArrowRight') {
+         if ((index + 1) === images.length) {
+            index = 0; 
+        } else {
+            index = index + 1;
+        }
+            modalWindowImage.src = galleryItems[index].original;
+         }
+         
+    }
+)
+}
 
 function onBackdropCloseClick (event) {
     if(event.target === event.currentTarget) {
@@ -40,30 +86,17 @@ function onGalleryLinkClick (event) {
     } 
         lightboxDiv.classList.add('is-open');
         const selectedImg = event.target;
+        index = +selectedImg.dataset.index;
         const originalImg = selectedImg.dataset.source;
         const description = selectedImg.alt;
         modalWindowImage.src = originalImg;
         modalWindowImage.alt = description;
+
+        imagesSlider(galleryItems);
+    
 }
 
 
-function createGalleryMarkup(array) {
-    return array.map(({preview, original, description}) => {
-        return `<li class="gallery__item">
-        <a
-          class="gallery__link"
-          href="${original}"
-        >
-          <img
-            class="gallery__image"
-            src="${preview}"
-            data-source="${original}"
-            alt="${description}"
-          />
-        </a>
-      </li>`
-    }).join('')
-}
 
 
 
